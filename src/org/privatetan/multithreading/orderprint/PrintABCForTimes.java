f6b809caf2dfb8f77b974693d8419b0648b6372a
privatetan <privatetantao@gmail.com>
2022-06-16 23:36:23 +0800

    Initial commit

---
+++ src/org/privatetan/multithreading/orderprint/PrintABCForTimes.java
@@ -0,0 +1,25 @@
+package org.privatetan.multithreading.orderprint;
+
+/**
+ * 三个线程分别打印 A，B，C，要求这三个线程一起运行，打印 n 次，输出形如“ABCABCABC....”的字符串；
+ */
+public class PrintABCForTimes {
+    public PrintABCForTimes() {
+    }
+
+    /**
+     * 打印次数
+     */
+    private int printTimes;
+
+    /**
+     * 线程替换状态，3个线程就是 0、1、2
+     */
+    private int threadState;
+
+    /**
+     * 锁
+     */
+    
+
+}
